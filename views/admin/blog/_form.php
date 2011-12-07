c45f555604c30482578150d375fffe2c6ea331ae
James Finley <jamesfinley@gmail.com>
2011-12-07 12:32:23 -0600

    Added image, fixed some bugs with autocomplete in tags.

--- views/admin/blog/_form.php
+++ views/admin/blog/_form.php
@@ -34,12 +34,12 @@
 			$(function () {
 				var tags = new Array();
 				<?php foreach ($tags as $tag): ?>
-				tags[] = ['<?=$tag->name?>'];
+				tags.push('<?=$tag->name?>');
 				<?php endforeach; ?>
 				var t;
 				t = new $.TextboxList('#starter_article_tags_field', {decode: function (o) {
 					return [];
-				}, bitsOptions: {stopEnter: true}, unique: true, plugins: {autocomplete: {onlyFromValues: false, inlineSuggest: false}}});
+				}, bitsOptions: {stopEnter: true}, unique: true, plugins: {autocomplete: {inlineSuggest: false}}});
 				<?php if ($article->persisted() && $article->tags): ?>
 					var val = $('#starter_article_tags_field').val();
 					val.split(',');
@@ -51,7 +51,7 @@
 				var autocomplete = t.plugins['autocomplete'];
 				autocomplete.setValues([
 				  <?php foreach ($tags as $tag): ?>
-				  '<?=$tag->name?>',
+				  ['<?=$tag->name?>', '<?=$tag->name?>'],
 				  <?php endforeach; ?>
 				]);
 			});
